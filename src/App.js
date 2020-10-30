9828fb2426cc355911a973df38e2206f82e2e229
Luis Muñoz <luenmuvel@gmail.com>
2020-10-29 20:46:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>English App</h1>
+        <p>Bienvenidos a esta aplicación para aprender ingles hecha por mi, <a href="https://www.linkedin.com/in/luise-munoz/">Luis Muñoz</a></p>
       </header>
+      <article>
+        <section>
+          <p>Pronto subiré más actualizaciones</p>
+        </section>
+      </article>
     </div>
   );
 }
